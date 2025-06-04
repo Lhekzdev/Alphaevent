19954f5fd394d941e961bc1513c83ce646d6c4fd
Your Name <olaseindeaduraleke001@gmail.com>
2025-06-04 12:40:04 +0100

    updating CreateEvent, and other pages

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -261,7 +261,8 @@ const images = [
                     <div className="text-red-500 text-[10px]">{errors.password}</div>
                   ) : null}
                 </div>
-<div  onClick={()=> redir('/OnboardEvent')} className="forget w-full flex items-end justify-end">
+                {/* onClick={()=> redir('/OnboardEvent')} */}
+<div   className="forget w-full flex items-end justify-end">
   
   <p className='text-[#FF6B6B] text-[12px] right-8 cursor-pointer' onClick={handleForgotPassword}>Forgot Password?</p>
 </div>
